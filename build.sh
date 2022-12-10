fe692c7d4a4e062cd75da85f470d43b6d82a2e9f
Raphael Mounier <mounierr07@gmail.com>
2022-12-10 13:52:04 +0100

    Build android13 r18

--- build.sh
+++ build.sh
@@ -68,8 +68,9 @@ WITHOUT_CHECK_API=true
 ORIGIN_FOLDER="$(dirname "$(readlink -f -- "$0")")"
 #export OUT_DIR=/home/iceows/build/A13
 
-#  repo init -u https://android.googlesource.com/platform/manifest -b android13-gsi
-repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r14
+# repo init -u https://android.googlesource.com/platform/manifest -b android13-gsi
+# _r16 for all pixel release
+repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r18
 
 prep_build() {
 	echo "Preparing local manifests"
