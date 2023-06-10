167d5c8d567fe3a99aa3648c9bdf0c07b56ab389
Raphael Mounier <mounierr07@gmail.com>
2023-06-10 14:30:41 +0200

    add test for already apply patch

--- apply-patches.sh
+++ apply-patches.sh
@@ -16,6 +16,12 @@ for project in $(cd $patches; echo *);do
 	pushd $p
 	git clean -fdx; git reset --hard
 	for patch in $patches/$project/*.patch;do
+		# Check if patch is already applied
+		if patch -f -p1 --dry-run -R < $patch > /dev/null; then
+            		printf "### ALREADY APPLIED: $patch \n";
+			continue
+		fi
+		
 		if git apply --check $patch;then
 			git am $patch
 		elif patch -f -p1 --dry-run < $patch > /dev/null;then
@@ -25,7 +31,7 @@ for project in $(cd $patches; echo *);do
 			git add -u
 			git am --continue
 		else
-			echo "Failed applying $patch"
+			printf "### FAILED APPLYING: $patch \n"
 			exit 1
 		fi
 	done
