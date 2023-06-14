fc06bebb0c15a571f83c13c2a4aa508aa3544709
Raphael Mounier <mounierr07@gmail.com>
2023-06-14 06:19:12 +0200

    move to r51 (QPR3)

--- build.sh
+++ build.sh
@@ -67,12 +67,11 @@ BUILD_DATE="$(date +%Y%m%d)"
 WITHOUT_CHECK_API=true
 ORIGIN_FOLDER="$(dirname "$(readlink -f -- "$0")")"
 
-
-#sexport OUT_DIR=/home/iceows/build/A13
+export OUT_DIR=/home/iceows/build/A13
 
 # repo init -u https://android.googlesource.com/platform/manifest -b android13-gsi
 # _r16 for all pixel release
-repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r49
+repo init -u https://android.googlesource.com/platform/manifest -b android-13.0.0_r51
 
 prep_build() {
 	echo "Preparing local manifests"
